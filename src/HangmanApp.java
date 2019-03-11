efd4e33d174c70abbb0f2e62b06696108a2b86e4
Josue Sencion <josue.sencion1@gmail.com>
2019-03-11 09:56:39 -0400

    3rd. The name of the "HangmanApp" class was refactored.

--- src/HangmanApp.java
+++ src/HangmanApp.java
@@ -4,7 +4,7 @@ Josue Sencion
 
 import java.util.Scanner;
 
-public class HagmanApp {
+public class HangmanApp {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
